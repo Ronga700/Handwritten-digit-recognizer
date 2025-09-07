d9a1c6da2f1ad4fb4edf4066f67d85046d1997ac
Ronga700 <macooggloog@gmail.com>
2025-09-07 17:17:41 +0400

    Update website.py

--- website.py
+++ website.py
@@ -3,6 +3,7 @@ from streamlit_drawable_canvas import st_canvas
 import numpy as np
 from PIL import Image
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 # Load your trained MNIST model
 # Make sure you have a model saved as 'mnist_model.h5'
@@ -36,7 +37,24 @@ if canvas_result.image_data is not None:
     img_array = 255 - img_array  # invert colors: white background → black background
     img_array = img_array / 255.0
     img_array = img_array.reshape(1, 28, 28, 1)
-    
+    img_tensor = tf.convert_to_tensor(img_array)
+
+    with tf.GradientTape() as tape:
+        tape.watch(img_tensor)
+        predictions1 = model(img_tensor)
+        predictions2 = CNN_model(img_tensor)
+        class_idx1 = tf.argmax(predictions1[0])
+        loss1 = predictions1[0][class_idx1]
+        class_idx2 = tf.argmax(predictions2[0])
+        loss2 = predictions2[0][class_idx2]
+    grads1 = tape.gradient(loss1, img_tensor)
+    grads2 = tape.gradient(loss2, img_tensor)
+# Take absolute value and max across channels if needed
+    saliency1 = tf.reduce_max(tf.abs(grads1), axis=-1)[0]
+    saliency2 = tf.reduce_max(tf.abs(grads2), axis=-1)[0]
+# Normalize for visualization
+    saliency1 = (saliency1 - tf.reduce_min(saliency1)) / (tf.reduce_max(saliency1) - tf.reduce_min(saliency1))
+    saliency2 = (saliency2 - tf.reduce_min(saliency2)) / (tf.reduce_max(saliency2) - tf.reduce_min(saliency2))
     # Predict digit
     prediction = model.predict(img_array)
     digit = np.argmax(prediction)
@@ -45,3 +63,5 @@ if canvas_result.image_data is not None:
     st.write("Predicted Digit for Baseline model:", digit)
     st.write("Predicted Digit for CNN model:", digit2)
     st.image(img, caption="Processed Image", width=140)
+    st.image(saliency1.numpy(), caption="Saliency Map", use_column_width=True)
+    st.image(saliency2.numpy(), caption="Saliency Map", use_column_width=True)
